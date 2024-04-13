2e4b7c291d5b2db27496349ccbbe53971df99a9b
AdityaPratapSinghR <adityapratap.github@gmail.com>
2024-04-13 23:48:50 +0530

    Added VideoPage

--- study_tube/lib/Screens/VideoPage.dart
+++ study_tube/lib/Screens/VideoPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
 class VideoPage extends StatefulWidget {
   const VideoPage({super.key});
 
@@ -7,8 +10,130 @@ class VideoPage extends StatefulWidget {
 }
 
 class _VideoPageState extends State<VideoPage> {
+  late String videoId;
+  late YoutubePlayerController _controller;
+  late TextEditingController _idController;
+  late TextEditingController _seekToController;
+
+  late PlayerState _playerState;
+  late YoutubeMetaData _videoMetaData;
+  bool _isPlayerReady = false;
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      videoId = ModalRoute.of(context)!.settings.arguments as String;
+      // After the playlistId is initialized, setState to rebuild the widget
+      setState(() {
+        _controller = YoutubePlayerController(
+          initialVideoId: videoId,
+          flags: const YoutubePlayerFlags(
+            mute: false,
+            autoPlay: true,
+            disableDragSeek: false,
+            loop: false,
+            isLive: false,
+            forceHD: false,
+            enableCaption: true,
+          ),
+        )..addListener(listener);
+        _idController = TextEditingController();
+        _seekToController = TextEditingController();
+        _videoMetaData = const YoutubeMetaData();
+        _playerState = PlayerState.unknown;
+      });
+    });
+  }
+
+  void listener() {
+    if (_isPlayerReady && mounted && !_controller.value.isFullScreen) {
+      setState(() {
+        _playerState = _controller.value.playerState;
+        _videoMetaData = _controller.metadata;
+      });
+    }
+  }
+
+  @override
+  void deactivate() {
+    // Pauses video while navigating to next page.
+    _controller.pause();
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _idController.dispose();
+    _seekToController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: YoutubePlayerBuilder(
+            onExitFullScreen: () {
+              // The player forces portraitUp after exiting fullscreen. This overrides the behaviour.
+              SystemChrome.setPreferredOrientations(DeviceOrientation.values);
+            },
+            player: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressIndicatorColor: Colors.blueAccent,
+              topActions: <Widget>[
+                const SizedBox(width: 8.0),
+                Expanded(
+                  child: Text(
+                    _controller.metadata.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                  ),
+                ),
+
+              ],
+              onReady: () {
+                _isPlayerReady = true;
+              },
+              onEnded: (data) {
+              },
+            ),
+            builder: (context, player) => Scaffold(
+              appBar: AppBar(
+                title: const Text(
+                  'Video',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              body: ListView(
+                children: [
+                  player,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(height: 10,),
+                        Text(_videoMetaData.title,style: TextStyle(color: Colors.black,),),
+                        SizedBox(height: 10,),
+                        Text(_videoMetaData.author,style: TextStyle(color: Colors.black)),
+                        SizedBox(height: 10,),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
