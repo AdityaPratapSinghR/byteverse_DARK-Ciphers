3b58bbbdcf3a426df35c04289f04346b4195aa3e
Rahul-Chaudhary-dev <rahulkumarchaudhary2003@gmail.com>
2024-04-14 00:09:49 +0530

    Home page with all the necessary core functionality

---
+++ study_tube/lib/Database/PlaylisDB.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class PlaylistDB{
+  String playlistId;
+  int progress;
+  PlaylistDB({required this.playlistId, required this.progress});
+}
---
+++ study_tube/lib/Database/PlaylistVideosDB.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+class PlaylistVideosDB{
+  String videoId;
+  int progress;
+  PlaylistVideosDB({required this.videoId, required this.progress});
+}
